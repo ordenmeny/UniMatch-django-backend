b227097d8e24056a11ce1802e0f583f076f41e6d
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-09-28 13:52:32 +0500

    added celery to run_uwsgi.sh

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -7,6 +7,9 @@ uv run manage.py makemigrations
 uv run manage.py migrate
 uv run manage.py collectstatic --noinput
 
+uv run celery -A users.celery worker --loglevel=info > /var/log/celery_worker.log 2>&1 &
+uv run celery -A users.celery beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler > /var/log/celery_beat.log 2>&1 &
+
 exec uwsgi \
   --chdir /djangoapp \
   --module djangoProject.wsgi \
@@ -18,8 +21,6 @@ exec uwsgi \
   --vacuum \
   --die-on-term \
   --buffer-size 32768
-
-
-# --buffer-size 16384 \      # 16KB
-# --post-buffering 8192 \    # 8KB
-# --max-request-size 52428800 # 50MB
+  # --buffer-size 16384 \      # 16KB
+  # --post-buffering 8192 \    # 8KB
+  # --max-request-size 52428800 # 50MB
