e11dd80a16d31eefcbbff2c6852c6129ebf91b03
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-10-04 13:20:48 +0500

    changed broker_url in celery.py

--- users/celery.py
+++ users/celery.py
@@ -10,15 +10,14 @@ app = Celery("proj")
 # TESTING = 'test' in sys.argv
 # TESTING = TESTING or 'test_coverage' in sys.argv or 'pytest' in sys.modules
 CELERY = {
-    "broker_url": "redis://localhost:6379/0",
+    "broker_url": "redis://redis:6379/0",
     # 'task_always_eager': TESTING,
     "timezone": settings.TIME_ZONE,
     "result_backend": "django-db",
     "result_extended": True,
-    'task_track_started': True,
-
-    'include': [
-        'users.celery_tasks',
+    "task_track_started": True,
+    "include": [
+        "users.celery_tasks",
     ],
 }
 
